af1b8cd18dd83dda10e1db07b6f85067af589b0a
Chenchen Zheng <chenchen.zheng@gmail.com>
2014-02-19 01:24:11 -0800

    add some more

--- AROne/AROne/ARAppDelegate.m
+++ AROne/AROne/ARAppDelegate.m
@@ -19,6 +19,9 @@
 
 @end
 @implementation ARAppDelegate
+// In the app delegate we create a constant string to be used as an event name
+static NSString* const kPAWLocationChangeNotification= @"kPAWLocationChangeNotification";
+
 
 // ****************************************************************************
 // App switching methods to support Facebook Single Sign-On.
@@ -28,7 +31,6 @@
   sourceApplication:(NSString *)sourceApplication
          annotation:(id)annotation {
   return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
-//  return [PFFacebookUtils handleOpenURL:url];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
@@ -84,97 +86,17 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo {
 }
 
 #pragma mark - private functions
-//- (void) setTabBarController
-//{
-//  //check if device is ipad or iphone
-//  NSString *deviceType = [UIDevice currentDevice].model;
-//  NSLog(@"deviceType is %@", deviceType);
-//  if([deviceType rangeOfString:@"iPad"].location == NSNotFound) {
-//    //  ccz: init Trabbar controller with height of 50, thinking not to display tab bar text
-//    self.tabBarController = [[AKTabBarController alloc] initWithTabBarHeight:40 position:AKTabBarPositionBottom];
-//  } else {
-//    self.tabBarController = [[AKTabBarController alloc] initWithTabBarHeight:60 position:AKTabBarPositionBottom];
-//  }
-//  
-//  ARSummaryViewController *summary = [[ARSummaryViewController alloc] initWithStyle:UITableViewStylePlain];
-//  UINavigationController *summaryNavigation = [[UINavigationController alloc] initWithRootViewController:summary];
-//  ARScoreboardViewController *scoreboard = [[ARScoreboardViewController alloc] initWithStyle:UITableViewStylePlain];
-//  UINavigationController *scoreboardNavigation = [[UINavigationController alloc] initWithRootViewController:scoreboard];
-//  UINavigationController *trailSummary = [[UINavigationController alloc] initWithRootViewController:[ARTrailSummaryViewController new]];
-//  
-//  
-//  [self.tabBarController setViewControllers:[NSMutableArray arrayWithObjects:summaryNavigation, scoreboardNavigation, trailSummary, nil]]; //FIXME: delete summaryTabGraph
-//  
-//  // Tab background Image
-//  [self.tabBarController setBackgroundImageName:@"TabBarBlue"];
-//  [self.tabBarController setSelectedBackgroundImageName:@"TabBarBlue"];
-//  
-//  
-//  // If needed, set cap insets for the background image
-//  [_tabBarController setBackgroundImageCapInsets:UIEdgeInsetsMake(4, 4, 4, 4)];
-//  
-//  // Tabs top embos Color
-//  [_tabBarController setTabEdgeColor:[UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:0.8]];
-//  
-//  // Tabs Colors settings
-//  [_tabBarController setTabColors:@[[UIColor colorWithRed:0.1 green:0.1 blue:0.1 alpha:0.0],
-//                                    [UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:1.0]]]; // MAX 2 Colors
-//  
-//  [_tabBarController setSelectedTabColors:@[[UIColor colorWithRed:0.7 green:0.7 blue:0.7 alpha:1.0],
-//                                            [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.0]]]; // MAX 2 Colors
-//  
-//  // Tab Stroke Color
-//  //   [_tabBarController setTabStrokeColor:[UIColor whiteColor]];
-//  
-//  // Icons Color settings
-//  [_tabBarController setIconColors:@[[UIColor colorWithRed:174.0/255.0 green:174.0/255.0 blue:174.0/255.0 alpha:1],
-//                                     [UIColor colorWithRed:228.0/255.0 green:228.0/255.0 blue:228.0/255.0 alpha:1]]]; // MAX 2 Colors
-//  
-//  // Icon Shadow
-//  //   [_tabBarController setIconShadowColor:[UIColor blackColor]];
-//  //   [_tabBarController setIconShadowOffset:CGSizeMake(0, 1)];
-//  
-//  [_tabBarController setSelectedIconColors:@[[UIColor colorWithRed:174.0/255.0 green:174.0/255.0 blue:174.0/255.0 alpha:1],
-//                                             [UIColor colorWithRed:228.0/255.0 green:228.0/255.0 blue:228.0/255.0 alpha:1]]]; // MAX 2 Colors
-//  
-//  [_tabBarController setSelectedIconOuterGlowColor:[UIColor whiteColor]];
-//  
-//  // Text Color
-//  [_tabBarController setTextColor:[UIColor colorWithRed:157.0/255.0 green:157.0/255.0 blue:157.0/255.0 alpha:1.0]];
-//  [_tabBarController setSelectedTextColor:[UIColor colorWithRed:228.0/255.0 green:228.0/255.0 blue:228.0/255.0 alpha:1.0]];
-//  
-//  // Text font
-//  //   [_tabBarController setTextFont:[UIFont fontWithName:@"Chalkduster" size:14]];
-//  
-//  // Hide / Show glossy on tab icons
-//  [_tabBarController setIconGlossyIsHidden:YES];
-//  
-//  // Enable / Disable pre-rendered image mode
-//  [_tabBarController setTabIconPreRendered:NO];
-//  
-//  
-//  // Uncomment the following lines to completely remove the border from all elements.
-//  /*
-//   [_tabBarController setTabEdgeColor:[UIColor clearColor]];
-//   [_tabBarController setTabInnerStrokeColor:[UIColor clearColor]];
-//   [_tabBarController setTabStrokeColor:[UIColor clearColor]];
-//   [_tabBarController setTopEdgeColor:[UIColor clearColor]];
-//   */
-//  
-//  // Uncomment the following to display centered image in the center of the tabbar, similar to Instagram.
-//  /*
-//   UIImage *img = [UIImage imageNamed:@"sample-center-image"];
-//   UIImageView *bottomCenterView = [[UIImageView alloc] initWithImage:img];
-//   CGRect rect = _tabBarController.view.frame;
-//   bottomCenterView.frame = CGRectMake(rect.size.width/2 - img.size.width/2, rect.size.height - img.size.height,
-//   img.size.width, img.size.height);
-//   [_tabBarController.view addSubview:bottomCenterView];
-//   */
-//  
-//  [self.window setRootViewController:self.tabBarController];
-//
-//  
-//}
+
+// We also add a method to be called when the location changes.
+// This is where we post the notification to all observers.
+- (void)setCurrentLocation:(CLLocation *)aCurrentLocation
+{
+  NSDictionary *userInfo = [NSDictionary dictionaryWithObject: aCurrentLocation
+                                                       forKey:@"location"];
+  [[NSNotificationCenter defaultCenter] postNotificationName:kPAWLocationChangeNotification
+                                                      object:nil
+                                                    userInfo:userInfo];
+}
 
 - (void) setAppTheme
 {
@@ -212,70 +134,6 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo {
     [UIFont fontWithName:@"Avenir-Medium" size:44/1.9],
     NSFontAttributeName,
     nil]];
-
-  
-  
-  //  [[UILabel appearance] setFont:[UIFont fontWithName:@"Big Caslon" size:18]];
-  //  [[UILabel appearanceWhenContainedIn: [UITableView class], nil] setFont:[UIFont fontWithName:@"Big Caslon" size:10]];
-  //  UIColor *progressColor = [UIColor colorWithRed:96/255.0f green:219/255.0f blue:255/255.0f alpha:1.0f];
-//  [[AKTabBar appearance] setBackgroundColor: barColor];
-  // for iOS 7
-  //  [[UINavigationBar appearance] setBarTintColor:barColor];
-  //  [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-  //  [[UITabBar appearance] setBarTintColor:barColor];
-  //  [[UITabBar appearance] setTintColor:[UIColor whiteColor]];
-  // for iOS 6
-//  [[UITabBar appearance] setBackgroundColor:barColor];
-  
-  
-  //  [[UIProgressView appearance] setTintColor:progressColor];
-  //  [[UIProgressView appearance] setTrackTintColor:[UIColor clearColor]];
-  
-  
-  
-  //  [[UITabBarItem appearance] setBackgroundImage:[[UIImage alloc] init]];
-  //   [barButtonName setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIFont boldSystemFontOfSize:15<=SIZE YOU WANT], UITextAttributeFont,nil] forState:UIControlStateNormal];
-  
-  //  tabbar stuff
-//  [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys: [UIColor whiteColor], UITextAttributeTextColor,nil] forState:UIControlStateNormal];
-//  UIColor *titleHighlightedColor = [UIColor lightGrayColor];
-//  [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys: titleHighlightedColor, UITextAttributeTextColor, nil] forState:UIControlStateSelected];
-//  [[UITabBar appearance] setShadowImage:[[UIImage alloc] init]];
-  
-//ccz: segment apperance
-//  UIImage *segmentSelected =
-//  [[UIImage imageNamed:@"topBackSelected.png"]
-//   resizableImageWithCapInsets:UIEdgeInsetsMake(0, 15, 0, 15)];
-//  UIImage *segmentUnselected =
-//  [[UIImage imageNamed:@"topback.png"]
-//   resizableImageWithCapInsets:UIEdgeInsetsMake(0, 15, 0, 15)];
-//  UIImage *segmentSelectedUnselected =
-//  [UIImage imageNamed:@"segcontrol_sel-uns.png"];
-//  UIImage *segUnselectedSelected =
-//  [UIImage imageNamed:@"segcontrol_uns-sel.png"];
-//  UIImage *segmentUnselectedUnselected =
-//  [UIImage imageNamed:@"segcontrol_uns-uns.png"];
-//
-//  [[UISegmentedControl appearance] setBackgroundImage:segmentUnselected
-//                                             forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-//  [[UISegmentedControl appearance] setBackgroundImage:segmentSelected
-//                                             forState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
-
-//  [[UISegmentedControl appearance] setDividerImage:segmentUnselectedUnselected
-//                               forLeftSegmentState:UIControlStateNormal
-//                                 rightSegmentState:UIControlStateNormal
-//                                        barMetrics:UIBarMetricsDefault];
-//  [[UISegmentedControl appearance] setDividerImage:segmentSelectedUnselected
-//                               forLeftSegmentState:UIControlStateSelected
-//                                 rightSegmentState:UIControlStateNormal
-//                                        barMetrics:UIBarMetricsDefault];
-//  [[UISegmentedControl appearance]
-//   setDividerImage:segUnselectedSelected
-//   forLeftSegmentState:UIControlStateNormal
-//   rightSegmentState:UIControlStateSelected
-//   barMetrics:UIBarMetricsDefault];
-//  [[UISegmentedControl appearance] setTintColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.6]];
-//  4c6974
   
   [shadow setShadowColor: [UIColor colorWithRed:0.0/255.0 green:51.0/255.0 blue:87.0/255.0 alpha:0.75]];
   [shadow setShadowOffset: CGSizeMake(0.0f, -0.5f)];
