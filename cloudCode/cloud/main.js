4b8c8f6ea138b010869de74d794f1b093c2e8ca7
Jerry Wu <jiachiw@Chenchens-MacBook-Pro-2.local>
2014-03-04 00:15:22 -0800

    blah

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -1,4 +1,3 @@
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
@@ -8,9 +7,9 @@ Parse.Cloud.define("hello", function(request, response) {
 Parse.Cloud.define("scoreBoard", function(request, response) {
 	var query = new Parse.Query("Summary");
 	query.equalTo("date", request.params.date);
-	query.find({
-		success:function(results) {
-			
-		}
-	})
+	var txt = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+	response.success(txt);
 }
