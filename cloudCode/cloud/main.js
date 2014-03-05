5a0e98af7a9a76e528c2ec9511ac0d966f747248
Chenchen Zheng <chenchen.zheng@gmail.com>
2014-03-04 23:02:09 -0800

    add comment

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -4,9 +4,12 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
+
+// { "maxSpeed" : [“date” : "3/4/13”,  "data" : [ {“user” : “chenchen zheng”, “maxSpeed” : “25.4” }, {obj2} ] ] }
 Parse.Cloud.define("scoreBoard", function(request, response) {
 	var query = new Parse.Query("Summary");
 	query.equalTo("date", request.params.date);
+	var results = {
 	var txt = '{ "employees" : [' +
 '{ "firstName":"John" , "lastName":"Doe" },' +
 '{ "firstName":"Anna" , "lastName":"Smith" },' +
