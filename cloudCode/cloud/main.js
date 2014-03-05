9a0569e1f441ea4d73bb11b4bffc28dc46403a5c
Jerry Wu <jiachiw@Chenchens-MacBook-Pro-2.local>
2014-03-04 23:54:57 -0800

    update main.js

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -8,11 +8,16 @@ Parse.Cloud.define("hello", function(request, response) {
 // { "maxSpeed" : [“date” : "3/4/13”,  "data" : [ {“user” : “chenchen zheng”, “maxSpeed” : “25.4” }, {obj2} ] ] }
 Parse.Cloud.define("scoreBoard", function(request, response) {
 	var query = new Parse.Query("Summary");
-	query.equalTo("date", request.params.date);
-	var results = {
-	var txt = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-	response.success(txt);
-}
+	//query.equalTo("date", request.params.date);
+	//query.equalTo("date", "2014-03-04");
+	query.find({
+		success: function(user_data_per_day){
+			response.success(user_data_per_day.length);
+		},
+		error: function() {
+   		 // error is an instance of Parse.Error.
+			response.error("error");
+  		}
+	});
+
+});
