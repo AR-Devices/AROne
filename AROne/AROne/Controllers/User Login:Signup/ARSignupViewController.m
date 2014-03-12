e37f962005fd997d95f713c59a4e62593f9e5ba6
Jerry Wu <jiachiw@Chenchens-MacBook-Pro-2.local>
2014-03-11 20:51:02 -0700

    change our signup page, but I think it’s missing password confirm page. and I have asked Xuege to confirm.

--- AROne/AROne/Controllers/User Login:Signup/ARSignupViewController.m
+++ AROne/AROne/Controllers/User Login:Signup/ARSignupViewController.m
@@ -9,6 +9,9 @@
 #import "ARSignupViewController.h"
 
 @interface ARSignupViewController () <UITextFieldDelegate>
+@property (nonatomic) UITextField *firstName;
+@property (nonatomic) UITextField *lastName;
+
 @property (nonatomic) UITextField *email;
 @property (nonatomic) UITextField *password;
 @property (nonatomic) UITextField *password_confirm;
@@ -42,66 +45,126 @@
 
 - (void) setSignupPage
 {
-  UIImage *arlogo = [UIImage imageNamed:@"arlogo"];
-  UIImageView *arlogoView = [[UIImageView alloc] initWithImage:arlogo];
-  UIView *arlogoFrame = [[UIView alloc]initWithFrame:CGRectMake(0, 0, arlogo.size.width,arlogo.size.height)];
-  arlogoFrame.center = CGPointMake(self.view.bounds.size.width/2, 80);
-  [arlogoFrame addSubview:arlogoView];
+  UIImage *signup_template = [UIImage imageNamed:@"signup_bg"];
+  UIImageView *signup_templateView = [[UIImageView alloc] initWithImage:signup_template];
+  UIView *signup_templateFrame = [[UIView alloc] initWithFrame:CGRectMake(5, 30, signup_template.size.width, signup_template.size.height)];
+  [signup_templateFrame addSubview:signup_templateView];
   
-  UIImage *signup_button = [UIImage imageNamed:@"login_button"];
-  UIImageView *signup_buttonView = [[UIImageView alloc] initWithImage:signup_button];
-  UIButton *signup_buttonFrame = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
-  [signup_buttonFrame addTarget:self action:@selector(loginAction:) forControlEvents:UIControlEventTouchUpInside];
-  signup_buttonFrame.center = CGPointMake(self.view.bounds.size.width/2, 350);
-  [signup_buttonFrame addSubview:signup_buttonView];
-  [signup_buttonFrame setTitle:@"Sign up" forState:UIControlStateNormal];
+  UIImage *photo_button = [UIImage imageNamed:@"addPhoto"];
+  UIImageView *photo_buttonView = [[UIImageView alloc] initWithImage:photo_button];
+  UIButton *photo_buttonFrame = [[UIButton alloc] initWithFrame:CGRectMake(signup_template.size.width-photo_button.size.width-5, 5, photo_button.size.width, photo_button.size.height)];
+  [photo_buttonFrame addTarget:self action:@selector(addPicture:) forControlEvents:UIControlEventTouchUpInside];
+  [photo_buttonFrame addSubview:photo_buttonView];
   
-  _email = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
+  [signup_templateFrame addSubview:photo_buttonFrame];
+  [self.view addSubview:signup_templateFrame];
+  
+  _firstName = [[UITextField alloc] initWithFrame:CGRectMake(10, 30, 200, 30)];
+  _firstName.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+  _firstName.leftViewMode = UITextFieldViewModeAlways;
+  _firstName.adjustsFontSizeToFitWidth = YES;
+  _firstName.delegate = self;
+  [_firstName setBackgroundColor:[UIColor clearColor]];
+  [_firstName setFont:[UIFont fontWithName:@"Avenir-Roman" size:15.0]];
+  [_firstName setTextColor:[UIColor orangeColor]];
+  [_firstName setPlaceholder:@"First Name"];
+  
+  [signup_templateFrame addSubview:_firstName];
+  
+  _lastName = [[UITextField alloc] initWithFrame:CGRectMake(10, 75, 200, 30)];
+  _lastName.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+  _lastName.leftViewMode = UITextFieldViewModeAlways;
+  _lastName.adjustsFontSizeToFitWidth = YES;
+  _lastName.delegate = self;
+  [_lastName setBackgroundColor:[UIColor clearColor]];
+  [_lastName setFont:[UIFont fontWithName:@"Avenir-Roman" size:15.0]];
+  [_lastName setTextColor:[UIColor orangeColor]];
+  [_lastName setPlaceholder:@"Last Name"];
+  
+  [signup_templateFrame addSubview:_lastName];
+  
+  _email = [[UITextField alloc] initWithFrame:CGRectMake(10, 120, 200, 30)];
   _email.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
   _email.leftViewMode = UITextFieldViewModeAlways;
-  _email.center = CGPointMake(self.view.bounds.size.width/2, 200);
   _email.adjustsFontSizeToFitWidth = YES;
   _email.delegate = self;
-  [_email setBackground:[UIImage imageNamed:@"input"]];
-  [_email setFont:[UIFont fontWithName:@"Avenir-Roman" size:11.0]];
+  [_email setBackgroundColor:[UIColor clearColor]];
+  [_email setFont:[UIFont fontWithName:@"Avenir-Roman" size:15.0]];
   [_email setTextColor:[UIColor orangeColor]];
-  [_email setPlaceholder:@"Email"];
+  [_email setPlaceholder:@"Email(User Name)"];
   
-  _password = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
+  [signup_templateFrame addSubview:_email];
+  
+  _password = [[UITextField alloc] initWithFrame:CGRectMake(10, 165, 200, 30)];
   _password.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
   _password.leftViewMode = UITextFieldViewModeAlways;
-  _password.center = CGPointMake(self.view.bounds.size.width/2, 250);
   _password.adjustsFontSizeToFitWidth = YES;
   _password.delegate = self;
-  [_password setBackground:[UIImage imageNamed:@"input"]];
-  [_password setFont:[UIFont fontWithName:@"Avenir-Roman" size:11.0]];
+  [_password setBackgroundColor:[UIColor clearColor]];
+  [_password setFont:[UIFont fontWithName:@"Avenir-Roman" size:15.0]];
   [_password setTextColor:[UIColor orangeColor]];
-  [_password setPlaceholder:@"Create Password"];
-  [_password setSecureTextEntry:YES];
+  [_password setPlaceholder:@"Password"];
   
-  _password_confirm = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
-  _password_confirm.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
-  _password_confirm.leftViewMode = UITextFieldViewModeAlways;
-  _password_confirm.center = CGPointMake(self.view.bounds.size.width/2, 300);
-  _password_confirm.adjustsFontSizeToFitWidth = YES;
-  _password_confirm.delegate = self;
-  [_password_confirm setBackground:[UIImage imageNamed:@"input"]];
-  [_password_confirm setFont:[UIFont fontWithName:@"Avenir-Roman" size:11.0]];
-  [_password_confirm setTextColor:[UIColor orangeColor]];
-  [_password_confirm setPlaceholder:@"Confirm Password"];
-  [_password_confirm setSecureTextEntry:YES];
-  
-
-  
-  [self.view addSubview:arlogoFrame];
-  [self.view addSubview:_email];
-  [self.view addSubview:_password];
-  [self.view addSubview:_password_confirm];
-  [self.view addSubview:signup_buttonFrame];
+  [signup_templateFrame addSubview:_password];
+//
+//  UIImage *signup_button = [UIImage imageNamed:@"login_button"];
+//  UIImageView *signup_buttonView = [[UIImageView alloc] initWithImage:signup_button];
+//  UIButton *signup_buttonFrame = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
+//  [signup_buttonFrame addTarget:self action:@selector(loginAction:) forControlEvents:UIControlEventTouchUpInside];
+//  signup_buttonFrame.center = CGPointMake(self.view.bounds.size.width/2, 350);
+//  [signup_buttonFrame addSubview:signup_buttonView];
+//  [signup_buttonFrame setTitle:@"Sign up" forState:UIControlStateNormal];
+//  
+//  _email = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
+//  _email.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+//  _email.leftViewMode = UITextFieldViewModeAlways;
+//  _email.center = CGPointMake(self.view.bounds.size.width/2, 200);
+//  _email.adjustsFontSizeToFitWidth = YES;
+//  _email.delegate = self;
+//  [_email setBackground:[UIImage imageNamed:@"input"]];
+//  [_email setFont:[UIFont fontWithName:@"Avenir-Roman" size:11.0]];
+//  [_email setTextColor:[UIColor orangeColor]];
+//  [_email setPlaceholder:@"Email"];
+//
+//  _password = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
+//  _password.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+//  _password.leftViewMode = UITextFieldViewModeAlways;
+//  _password.center = CGPointMake(self.view.bounds.size.width/2, 250);
+//  _password.adjustsFontSizeToFitWidth = YES;
+//  _password.delegate = self;
+//  [_password setBackground:[UIImage imageNamed:@"input"]];
+//  [_password setFont:[UIFont fontWithName:@"Avenir-Roman" size:11.0]];
+//  [_password setTextColor:[UIColor orangeColor]];
+//  [_password setPlaceholder:@"Create Password"];
+//  [_password setSecureTextEntry:YES];
+//  
+//  _password_confirm = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, signup_button.size.width, signup_button.size.height)];
+//  _password_confirm.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+//  _password_confirm.leftViewMode = UITextFieldViewModeAlways;
+//  _password_confirm.center = CGPointMake(self.view.bounds.size.width/2, 300);
+//  _password_confirm.adjustsFontSizeToFitWidth = YES;
+//  _password_confirm.delegate = self;
+//  [_password_confirm setBackground:[UIImage imageNamed:@"input"]];
+//  [_password_confirm setFont:[UIFont fontWithName:@"Avenir-Roman" size:11.0]];
+//  [_password_confirm setTextColor:[UIColor orangeColor]];
+//  [_password_confirm setPlaceholder:@"Confirm Password"];
+//  [_password_confirm setSecureTextEntry:YES];
+//  
+//
+//  
+//  [self.view addSubview:arlogoFrame];
+//  [self.view addSubview:_email];
+//  [self.view addSubview:_password];
+//  [self.view addSubview:_password_confirm];
+//  [self.view addSubview:signup_buttonFrame];
 
 
 }
-
+- (void) addPicture: (id) sender
+{
+  UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"ADD PICTURE" message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
+  [alertView show];
+}
 - (void) loginAction: (id) sender
 {
   [self.email resignFirstResponder];
