31a00d47ae1f9d1e7ae4f51ae6f3be1a1ed90b56
Jerry Wu <jerrywujiachi>
2014-04-21 22:23:58 -0700

    addDesending to scoreboard

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -11,7 +11,7 @@ Parse.Cloud.define("scoreBoard", function(request, response) {
 	query.include("player");
 	//query.equalTo("displayName", "danny");
 //	query.equalTo("date", request.params.date);
-//	query.addDescending(request.params.datatype);
+	query.addDescending(request.params.datatype);
 //	query.select("displayName", request.params.datatype);
 	query.greaterThanOrEqualTo("date", request.params.startDate);
 	query.lessThanOrEqualTo("date", request.params.endtDate);
