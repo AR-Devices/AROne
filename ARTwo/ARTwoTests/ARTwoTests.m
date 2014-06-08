e6205a2be8f6570408fa5f7db536faee606814ac
Chenchen Zheng <chenchen.zheng@gmail.com>
2014-06-07 18:54:46 -0700

    add ARTwo

---
+++ ARTwo/ARTwoTests/ARTwoTests.m
@@ -0,0 +1,39 @@
+//
+//  ARTwoTests.m
+//  ARTwoTests
+//
+//  Created by Chenchen Zheng on 6/7/14.
+//  Copyright (c) 2014 Chenchen Zheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ARTwoTests : XCTestCase
+
+@end
+
+@implementation ARTwoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
